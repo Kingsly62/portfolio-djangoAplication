85ca617a3f27912bd680b19d396afa565ac331f8
Kingsly62 <clinton.makori@student.moringaschool.com>
2022-02-23 01:09:03 +0300

    initial commit

---
+++ home/views.py
@@ -0,0 +1,20 @@
+from cgitb import html
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def projects(request):
+    return HttpResponse('This is my projects Home page(/)')
+
+
+def contact(request):
+    return HttpResponse('This is my contact Home page(/)')
